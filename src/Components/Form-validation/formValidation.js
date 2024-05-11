a1bd1bab7c59c5814397c5e2208234b2755ae917
noyon674 <sarker35-674@diu.edu.bd>
2024-05-11 13:17:54 +0600

    new practice of formik

--- src/Components/Form-validation/formValidation.js
+++ src/Components/Form-validation/formValidation.js
@@ -1,34 +1,51 @@
 import React, {useState} from 'react';
-import { useFormik } from 'formik';
+import {useFormik} from 'formik';
 
 function FormValidation() {
+
     const formik = useFormik({
         initialValues:{
             name:'',
             email:'',
             pass:''
         },
-        onSubmit:(values, {resetForm})=>{
-            console.log(values);
+        onSubmit: (values, {resetForm})=>{
+            console.log(values)
             resetForm({values: ''})
         }
-    })
+        
+    });
 
   return (
     <div className='center'>
-        <h2>Form validation with formik</h2>
+        <h1>Form Validation with Formik</h1>
         <form action='' onSubmit={formik.handleSubmit}>
             <div>
                 <label>Name:</label>
-                <input type='text' name='name' value={formik.values.name} onChange={formik.handleChange} re/>
+                <input 
+                type='text'
+                name='name'
+                value={formik.values.name}
+                onChange={formik.handleChange}
+                required/>
             </div>
             <div>
                 <label>Email:</label>
-                <input type='email' name='email' value={formik.values.email} onChange={formik.handleChange}/>
+                <input 
+                type='email'
+                name='email'
+                value={formik.values.email}
+                onChange={formik.handleChange}
+                required/>
             </div>
             <div>
                 <label>Password:</label>
-                <input type='password' name='pass' value={formik.values.pass} onChange={formik.handleChange}/>
+                <input 
+                type='password'
+                name='pass'
+                value={formik.values.pass}
+                onChange={formik.handleChange}
+                required/>
             </div>
             <button type='submit'>Send</button>
         </form>
@@ -36,4 +53,4 @@ function FormValidation() {
   )
 }
 
-export default FormValidation;
+export default FormValidation
